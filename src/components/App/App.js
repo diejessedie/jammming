aba29d3e3319efca2c80586d2004d58e7ab09b59
Jess <jjohnson4927@gmail.com>
2017-11-30 19:45:25 -0600

    create components, css

---
+++ src/components/App/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./App.css";
+import SearchBar from '../SearchBar/SearchBar.js';
+import SearchResults from '../SearchResults/SearchResults.js';
+import Playlist from '../Playlist/Playlist.js';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [{name: ""},{artist: ""},{album: ""}]
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults}/>
+            <Playlist />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Tracklist/Tracklist.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Tracklist.css";
+import Track from '../Track/Track.js';
+
+export class Tracklist extends React.Component {
+  render() {
+    return (
+      <div className="TrackList">
+</div>
+    );
+  }
+}
