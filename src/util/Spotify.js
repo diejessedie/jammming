e782dd25932a22d870b8c4220214561617c976e0
Jess <jjohnson4927@gmail.com>
2017-12-04 22:05:44 -0600

    fix redirect URI error in Spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,6 @@
 let accessToken;
 const client_id = '066245d772114cb68fef842c09a0ec96';
-const redirect_uri = 'http://localhost:3000/';
+const redirect_uri = 'http://localhost:3000/callback';
 
 //Create a Spotify Module
 const Spotify = {
@@ -8,22 +8,17 @@ const Spotify = {
     if (accessToken) {
       return accessToken;
     }
-    const newAccessToken = window.location.href.match(/access_token=([^&]*)/);
-    const newExpiresIn = window.location.href.match(/expires_in=([^&]*)/);
 
-    if (newAccessToken && newExpiresIn) {
-      accessToken = newAccessToken[1];
-      let expiresIn = Number(newExpiresIn[1]);
-      //Clear the parameters from the URL, so the app doesn't try grabbing the access token after it has expired
-      window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
-      window.history.pushState("Access Token", null, "/");
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      let expiresIn = Number(expiresInMatch[1]);
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState("Access Token", null, "/"); // This clears the parameters, allowing us to grab a new access token when it expires.
       return accessToken;
     } else {
-      let accessUrl = `https://accounts.spotify.com/authorize?client_id=${
-        client_id
-      }&response_type=token&scope=playlist-modify-public&show_dialog=true&redirect_uri=${
-        redirect_uri
-      }`;
+      let accessUrl = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirect_uri}`;
       window.location = accessUrl;
     }
   },
