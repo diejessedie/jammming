8b1305826eb5572de5203bd94ca82210ee7bf549
Jess <jjohnson4927@gmail.com>
2017-12-08 19:45:18 -0600

    new approach to enter search, unsuccessful

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,12 +4,9 @@ import "./SearchBar.css";
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-<<<<<<< HEAD
     this.state = {
       term: ""
     };
-=======
->>>>>>> parent of eb221b2... reset savePlaylist state, initialize search term state
     this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
@@ -27,16 +24,21 @@ class SearchBar extends Component {
     });
   }
 
+  handleKeyPress(e) {
+    if (e.key === `Enter`) {
+      this.search();
+    }
+  }
+
   render() {
     return (
       <div className="SearchBar">
         <input
           placeholder="Enter A Song, Album, or Artist"
           onChange={this.handleTermChange}
+          onKeyPress={this.handleKeyPress}
         />
-        <a onClick={this.search}>
-          SEARCH
-        </a>
+        <a onClick={this.search}>SEARCH</a>
       </div>
     );
   }
